5d7ec230588b9582c6e5ddd5500554dfb54883db
Hajime0610 <kagetora0610@gmail.com>
2015-06-24 11:28:49 +0900

    atukunレイアウト

--- arukun/arukun/GameScene.swift
+++ arukun/arukun/GameScene.swift
@@ -11,32 +11,22 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        self.backgroundColor = UIColor(red: 0.7, green: 0.7, blue: 1.0, alpha: 1.0)
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
+        myLabel.text = "メイン画面";
+        myLabel.fontColor = UIColor.blackColor()
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        myLabel.name = "no"
         
         self.addChild(myLabel)
+
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
+
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
--- arukun/arukun/GameViewController2.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GameViewController2.swift
-//  arukun
-//
-//  Created by 坂本一 on 2015/06/19.
-//  Copyright (c) 2015年 chikaratada. All rights reserved.
-//
-
-import Foundation
