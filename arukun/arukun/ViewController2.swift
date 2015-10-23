50ce919f25cd1caf5f471642f6d01fb0fbfb4667
Chikara0176 <chikara.1993@gmail.com>
2015-10-23 12:02:10 +0900

    コンフリクト解消

---
+++ arukun/arukun/ViewController2.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController2.swift
+//  kore test 2
+//
+//  Created by kenseikamii on 2015/10/03.
+//  Copyright © 2015年 kenseikamii. All rights reserved.
+//
+import Foundation
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImg: UIImage?
+    var selectedlbl: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = selectedlbl
+        
+        //２画面目拡大画像
+        imageView.image = selectedImg
+        
+        // 画像のアスペクト比を維持しUIImageViewサイズに収まるように表示
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
